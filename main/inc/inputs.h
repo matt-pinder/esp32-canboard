eb633daa4638c61e85a85dcc958df558de91b104
f0xxxxxy <jordan@f0xy.net>
2025-06-27 23:26:31 +0100

    NTC Table

--- main/inc/inputs.h
+++ main/inc/inputs.h
@@ -28,3 +28,11 @@ extern uint16_t scaled_pressures[2];
 int8_t getSensorTemperature(int v_mv, int r_pullup, int v_ref_mv);
 uint16_t getSensorPressure(int v_mv, int v_min_mv, int v_max_mv, float p_min, float p_max);
 uint16_t getScaledMillivolts(adc_channel_t channel, bool scaled);
+
+static const ntc_point_t ntc_table[] = {
+    { -40, 45313 }, { -30, 26114 }, { -20, 15462 }, { -10,  9397 },
+    {   0,  5896 }, {  10,  3792 }, {  20,  2500 }, {  30,  1707 },
+    {  40,  1175 }, {  50,   834 }, {  60,   596 }, {  70,   436 },
+    {  80,   323 }, {  90,   243 }, { 100,   187 }, { 110,   144 },
+    { 120,   113 }, { 130,    89 }, { 140,    71 }
+};
